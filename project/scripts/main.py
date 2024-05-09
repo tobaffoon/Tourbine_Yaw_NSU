fe7c1fa305dfc233690546ae5d5e96056729c948
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-05-10 03:27:04 +0700

    Implement python input and output

--- project/scripts/main.py
+++ project/scripts/main.py
@@ -5,19 +5,31 @@
 """
 import os
 import matplotlib.pyplot as plt
-from openfast_toolbox.io import FASTOutputFile
+from openfast_toolbox.case_generation import runner
+from openfast_toolbox.io import FASTInputFile, FASTOutputFile
 
+YawErrorStrat = 0
+WindDeltaStrat = 1
+
+YawStrat = YawErrorStrat
+YawArg = 0.1
 # Get current directory so this script can be called from any location
 scriptDir = os.path.dirname(__file__)
 
-fastoutFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/reg_tests/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/delta-0_3-real1.outb')
+fastinFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.fst')
+yawParamsFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/build/bin/yaw_control_params.dat"')
+openFAST_EXE = os.path.join(scriptDir, '../openfast_yaw_NSU/build/bin/openfast_x64.exe')  
+runner.run_cmd([fastinFilename, str(YawStrat), str(YawArg)], openFAST_EXE, showOutputs=True)
+
+fastoutFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.outb')
 df = FASTOutputFile(fastoutFilename).toDataFrame()
 print(df.keys())
 time  = df['Time_[s]']
-Omega = df['GenPwr_[kW]']
-plt.plot(time, Omega)
+Power = df['GenPwr_[kW]']
+plt.plot(time, Power)
 plt.xlabel('Time [s]')
 plt.ylabel('GenPwr [kW]')
+print(sum(Power))
 
 if __name__ == '__main__':
     plt.show()
