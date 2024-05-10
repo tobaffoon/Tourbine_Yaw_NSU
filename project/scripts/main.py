e61149ad8774f01bcedc70a9375efba6b6b15d90
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-05-10 21:15:52 +0700

    Complete both strategies minimization

--- project/scripts/main.py
+++ project/scripts/main.py
@@ -12,7 +12,7 @@ def run_openFAST_yaw(YawArgument, YawStrategy):
     fastinFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.fst')
     openFAST_EXE = os.path.join(scriptDir, '../openfast_yaw_NSU/build/bin/openfast_x64.exe')  
     out = runner.run_cmd([fastinFilename, str(YawStrategy), str(YawArgument[0])], openFAST_EXE, showOutputs=True)
-    if out.returncode == 1:
+    if out.returncode != 0:
         return 0
     
     fastoutFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.outb')
@@ -20,6 +20,32 @@ def run_openFAST_yaw(YawArgument, YawStrategy):
     Power = sum(df['GenPwr_[kW]'])
     return Power
 
+def neg_yaw(YawArgument, YawStrategy):
+    return -run_openFAST_yaw(YawArgument, YawStrategy)
+
 if __name__ == '__main__':
-    res = minimize(run_openFAST_yaw, 0.1, args=0, method='Nelder-Mead')
-    print(res.x, res.success, res.message)
+    arg0 = 0.2
+    strat = YawErrorStrat
+    res_list = []
+    while arg0 <= 1.0:
+        res = minimize(neg_yaw, arg0, args=strat, method='Nelder-Mead', bounds=[(0, 3)])
+        if res.success :
+            res_list.append(("s", arg0, res.x[0], -res.fun))
+        else:
+            res_list.append(("f", arg0, res.x[0], -res.fun))
+        arg0 += 0.1
+    with open("res.txt", "w") as fout:
+        fout.write("Yaw Error: " + str(res_list) + "\n")
+        
+    arg0 = 13.0
+    strat = WindDeltaStrat
+    res_list = []
+    while arg0 <= 23.0:
+        res = minimize(neg_yaw, arg0, args=strat, method='Nelder-Mead', bounds=[(13, 23)], options={"maxiter": 20})
+        if res.success :
+            res_list.append(("s", arg0, res.x[0], -res.fun))
+        else:
+            res_list.append(("f", arg0, res.x[0], -res.fun))
+        arg0 += 5.0
+    with open("res.txt", "a") as fout:
+        fout.write("wind speed: " + str(res_list) + "\n")
