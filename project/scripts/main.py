f19fefa4223ca15c29305e8d0700ba8c7b127352
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-05-12 13:48:58 +0700

    Remove openfast source code

--- project/scripts/main.py
+++ project/scripts/main.py
@@ -1,5 +1,4 @@
 import os
-import matplotlib.pyplot as plt
 from openfast_toolbox.case_generation import runner
 from openfast_toolbox.io import FASTInputFile, FASTOutputFile
 from scipy.optimize import minimize
@@ -9,8 +8,8 @@ WindDeltaStrat = 1
 scriptDir = os.path.dirname(__file__)
 
 def run_openFAST_yaw(YawArgument, YawStrategy):
-    fastinFilename = os.path.join(scriptDir, '../openfast_yaw_NSU/r-test/glue-codes/openfast/5MW_Land_DLL_WTurb/5MW_Land_DLL_WTurb.fst')
-    openFAST_EXE = os.path.join(scriptDir, '../openfast_yaw_NSU/build/bin/openfast_x64.exe')  
+    fastinFilename = os.path.join(scriptDir, '../openfast/turbine/5MW_Land_DLL_WTurb.fst')
+    openFAST_EXE = os.path.join(scriptDir, '../openfast/openfast_x64.exe')  
     out = runner.run_cmd([fastinFilename, str(YawStrategy), str(YawArgument[0])], openFAST_EXE, showOutputs=True)
     if out.returncode != 0:
         return 0
